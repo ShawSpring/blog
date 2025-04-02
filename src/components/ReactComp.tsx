98a1d7ce4e8fe459fb7c68bf036b2f69cead527f
ShawSpring <chunlaixiao@163.com>
2025-04-03 01:22:37 +0800

    fix:lint style

--- src/components/ReactComp.tsx
+++ src/components/ReactComp.tsx
@@ -1,11 +1,11 @@
-
 function ReactComp({ title }: { title: string }) {
 	return (
 		<div className="w-full m-0 border-l-2 border-teal-600 flex flex-col gap-2 py-2 [padding-left:1rem]">
-			<div role="heading" aria-level={2} className="text-3xl font-bold">{title}</div>
-			<div className="rounded-md text-center">
-				this is a react component
+			{/* biome-ignore lint/a11y/useSemanticElements: <explanation> */}
+			<div role="heading" aria-level={2} className="text-3xl font-bold">
+				{title}
 			</div>
+			<div className="rounded-md text-center">this is a react component</div>
 		</div>
 	);
 }
